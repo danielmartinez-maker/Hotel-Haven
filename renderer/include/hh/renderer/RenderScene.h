77503d4364e27ad31c579b0fe2235a0b29dcf045
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-09 10:51:33 -0600

    feat(renderer): add mesh scene instance DTOs

--- renderer/include/hh/renderer/RenderScene.h
+++ renderer/include/hh/renderer/RenderScene.h
@@ -3,6 +3,7 @@
 #include <optional>
 #include <vector>
 
+#include "hh/renderer/AssetHandle.h"
 #include "hh/renderer/MathTypes.h"
 
 namespace hh::renderer {
@@ -29,8 +30,25 @@ struct BoxRenderItem {
     Aabb bounds{};
 };
 
+struct MeshTransform {
+    Vec3 translation{};
+    Vec3 scale{1.0f, 1.0f, 1.0f};
+    float yawRadians{};
+};
+
+struct MeshRenderItem {
+    AssetHandle asset{};
+    MeshTransform transform{};
+    Color tint{1.0f, 1.0f, 1.0f, 1.0f};
+    int floorId{};
+    RenderCategory category{RenderCategory::Object};
+    Aabb bounds{};
+    bool translucent{};
+};
+
 struct RenderScene {
     std::vector<BoxRenderItem> items;
+    std::vector<MeshRenderItem> meshes;
     std::optional<Vec3> focusTarget;
     int activeFloor{};
 };
@@ -40,10 +58,18 @@ struct ComposedBox {
     bool cutaway{};
 };
 
+struct ComposedMesh {
+    MeshRenderItem item;
+    bool cutaway{};
+};
+
 struct ComposedScene {
     std::vector<ComposedBox> opaque;
     std::vector<ComposedBox> translucent;
     std::vector<ComposedBox> wireframe;
+    std::vector<ComposedMesh> opaqueMeshes;
+    std::vector<ComposedMesh> translucentMeshes;
+    std::vector<ComposedMesh> wireframeMeshes;
 };
 
 }  // namespace hh::renderer
