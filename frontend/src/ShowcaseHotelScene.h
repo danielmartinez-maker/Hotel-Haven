a4d9b486221d227fb5d2c0bb8953018920bfc325
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-09 09:41:56 -0600

    feat: add showcase ambient update contract

--- frontend/src/ShowcaseHotelScene.h
+++ frontend/src/ShowcaseHotelScene.h
@@ -7,6 +7,7 @@ namespace hh::frontend {
 class ShowcaseHotelScene {
 public:
     [[nodiscard]] static hh::renderer::RenderScene build();
+    static void updateAmbient(hh::renderer::RenderScene& scene, float elapsedSeconds) noexcept;
 };
 
 }  // namespace hh::frontend
