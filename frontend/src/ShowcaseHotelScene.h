782720cd7eb8db00219cbf46daf24fca07cf0a4a
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-09 09:16:08 -0600

    feat: define canonical showcase hotel scene

---
+++ frontend/src/ShowcaseHotelScene.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "hh/renderer/RenderScene.h"
+
+namespace hh::frontend {
+
+class ShowcaseHotelScene {
+public:
+    [[nodiscard]] static hh::renderer::RenderScene build();
+};
+
+}  // namespace hh::frontend
