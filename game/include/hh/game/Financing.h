dbea43796d5bd3cf984058fcae7bc9e5eca89e59
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-09 21:13:49 -0600

    feat: add quoted amortizing loan model

--- game/include/hh/game/Financing.h
+++ game/include/hh/game/Financing.h
@@ -36,6 +36,16 @@ struct LoanOffer {
   std::int64_t minimumCashCents{};
 };
 
+struct LoanQuote {
+  bool ok{};
+  std::string reason;
+  int paymentCount{};
+  std::int64_t nextPaymentCents{};
+  std::int64_t totalInterestCents{};
+  std::int64_t totalRepaymentCents{};
+  explicit operator bool() const noexcept { return ok; }
+};
+
 struct LoanResult {
   bool ok{};
   std::string reason;
@@ -63,6 +73,7 @@ struct FinancingSnapshot {
 
 class FinancingSystem {
 public:
+  [[nodiscard]] LoanQuote quoteLoan(const LoanOffer &offer) const;
   [[nodiscard]] LoanResult acceptLoan(const LoanOffer &offer,
                                       std::int64_t currentCashCents);
   [[nodiscard]] DebtServiceResult processDay(int day,
@@ -81,6 +92,10 @@ private:
     std::int64_t remainingPrincipalCents{};
     int paymentsRemaining{};
     int nextPaymentDay{};
+    std::int64_t scheduledPaymentCents{};
+    std::int64_t quotedTotalInterestCents{};
+    std::int64_t interestPaidCents{};
+    bool legacyEqualPrincipal{};
   };
 
   std::vector<ActiveLoan> loans_;
@@ -90,8 +105,11 @@ private:
   std::int64_t missedObligationCents_{};
   bool covenantBreach_{};
 
-  [[nodiscard]] static std::int64_t interestDue(const ActiveLoan &loan);
-  [[nodiscard]] static std::int64_t principalDue(const ActiveLoan &loan);
+  [[nodiscard]] static int paymentCount(const LoanOffer &offer);
+  [[nodiscard]] static std::int64_t periodicInterestDue(const ActiveLoan &loan);
+  [[nodiscard]] static std::int64_t legacyInterestDue(const ActiveLoan &loan);
+  [[nodiscard]] static std::int64_t legacyPrincipalDue(const ActiveLoan &loan);
+  [[nodiscard]] static std::int64_t nextPaymentDue(const ActiveLoan &loan);
 };
 
 } // namespace hh::game
