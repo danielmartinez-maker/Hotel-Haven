00fa8a8830a274aa15f79c57039caa2d36f83857
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-10 23:03:35 -0600

    refactor(final07): isolate native construction tool ids

---
+++ game/app/BuildTool.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace hh::client {
+
+enum class Tool {
+  Inspect,
+  Bedroom,
+  Floor,
+  Wall,
+  Door,
+  Entrance,
+  Desk,
+  Closet,
+  Stairs,
+  Erase,
+  Bathroom,
+  StaffRoom,
+  Lobby
+};
+
+} // namespace hh::client
