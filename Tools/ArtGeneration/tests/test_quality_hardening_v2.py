035fd4a13e233ce65768034dac645b7a41bfa9a4
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-24 16:41:57 -0600

    test(assets): align preview binding order with 34 core visuals

--- Tools/ArtGeneration/tests/test_quality_hardening_v2.py
+++ Tools/ArtGeneration/tests/test_quality_hardening_v2.py
@@ -168,7 +168,7 @@ def test_runtime_binding_preview_contract_tracks_shipping_assets():
     assert len(ids) == len(set(ids))
     assert ids[:3] == ['HH_A030', 'HH_A012', 'HH_A057']
     assert ids[3:9] == [f'HH_A{i:03d}' for i in range(111, 117)]
-    assert ids[28:58] == [f'HH_A{i:03d}' for i in range(451, 481)]
+    assert ids[34:64] == [f'HH_A{i:03d}' for i in range(451, 481)]
     assert ids[-6:] == [
         'HH_A481', 'HH_A482',
         'HH_A487', 'HH_A488',
