867d720735fdfb09f08c562b067ae8b25dfe6287
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-24 15:22:23 -0600

    perf: add reusable scene composition buffer

--- renderer/include/hh/renderer/SceneComposer.h
+++ renderer/include/hh/renderer/SceneComposer.h
@@ -7,6 +7,13 @@ namespace hh::renderer {
 
 class SceneComposer {
 public:
+    void compose(
+        const RenderScene& scene,
+        FloorContextMode contextMode,
+        WallRenderMode wallMode,
+        Vec3 cameraWorldPosition,
+        ComposedScene& result) const;
+
     [[nodiscard]] ComposedScene compose(
         const RenderScene& scene,
         FloorContextMode contextMode,
