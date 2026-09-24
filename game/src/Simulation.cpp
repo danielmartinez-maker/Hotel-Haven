d2cf26982d39ece884ff5f50c217737daca5163d
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-24 14:05:46 -0600

    fix(final03): account onboarding as labor cost

--- game/src/Simulation.cpp
+++ game/src/Simulation.cpp
@@ -1913,6 +1913,7 @@ HireResult Simulation::hireApplicant(ApplicantId applicantId) {
   impl_->people.push_back(p);
   impl_->consumedApplicantIds.push_back(found->id);
   impl_->economy.cashCents -= impl_->onboardingCostCents;
+  impl_->economy.payrollCents += impl_->onboardingCostCents;
   return {true, "Applicant hired", p.id, found->id};
 }
 CommandResult Simulation::fireStaff(EntityId id) {
